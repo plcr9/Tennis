e6f6370fa33e9f42436732aa186697ceb9c36c79
Paul <118978940+plcr9@users.noreply.github.com>
2023-07-11 17:43:05 +0100

    Create play_tennis.py

---
+++ play_tennis.py
@@ -0,0 +1,9 @@
+from tennis import Player, Match
+
+alcaraz = Player("Carlos Alcaraz", 2000)
+rune = Player("Holger Rune", 2000)
+
+print(alcaraz.name)
+print(alcaraz.ranking_points)
+print(rune.name)
+print(rune.ranking_points)
