607d3777b0047a98b35971ba3062b5c025a40170
Paul <118978940+plcr9@users.noreply.github.com>
2023-07-11 17:49:14 +0100

    Update play_tennis.py

--- play_tennis.py
+++ play_tennis.py
@@ -9,6 +9,8 @@ print(rune.name)
 print(rune.ranking_points)
 
 alcaraz.update_ranking_points(2000)
+print(alcaraz.name)
 print(alcaraz.ranking_points)
 rune.update_ranking_points(1200)
+print(rune.name)
 print(rune.ranking_points)
