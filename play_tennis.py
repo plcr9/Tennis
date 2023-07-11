fb51c5f5dcc10a6ffeb691fb8c32cce5c3a8f06c
Paul <118978940+plcr9@users.noreply.github.com>
2023-07-11 17:48:14 +0100

    Update play_tennis.py

--- play_tennis.py
+++ play_tennis.py
@@ -7,3 +7,8 @@ print(alcaraz.name)
 print(alcaraz.ranking_points)
 print(rune.name)
 print(rune.ranking_points)
+
+alcaraz.update_ranking_points(2000)
+print(alcaraz.ranking_points)
+rune.update_ranking_points(1200)
+print(rune.ranking_points)
