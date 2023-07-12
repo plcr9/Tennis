af5c0eae64ac5c4ce84c3a1635dd066cbb4bedb5
Paul <118978940+plcr9@users.noreply.github.com>
2023-07-12 12:52:34 +0100

    Update play_tennis.py

--- play_tennis.py
+++ play_tennis.py
@@ -19,8 +19,8 @@ test_match = Match(alcaraz, rune)
 test_set = Set(test_match)
 test_game = Game(test_set)
 
-print(test_game)
-print(repr(test_game))
+test_game.score_point(alcaraz)
 
-print(f"{alcaraz}")
-print(f"{alcaraz!r}")
+print(test_game.players)
+print(test_game.is_running())
+print(test_game.get_winner())
