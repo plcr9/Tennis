d9b16066e19fab1a4bd14453d706204ec80366eb
Paul <118978940+plcr9@users.noreply.github.com>
2023-07-12 13:34:56 +0100

    Update play_tennis.py

--- play_tennis.py
+++ play_tennis.py
@@ -15,7 +15,7 @@ rune.update_ranking_points(1200)
 print(rune.name)
 print(rune.ranking_points)
 
-test_match = Match(alcaraz, rune)
-test_set = Set(test_match)
-
-test_set.play_game()
+while test_set.is_running():
+  test_set.play_game()
+  print(str(test_set))
+print(test_set.winner)
