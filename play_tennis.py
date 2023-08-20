00cd2bc873028476f4708333171a56b0fe865b88
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-20 14:02:33 +0100

    Update play_tennis.py

--- play_tennis.py
+++ play_tennis.py
@@ -6,3 +6,7 @@ rune = Player("Holger Rune", 2000)
 test_match = Match(alcaraz,rune)
 
 test_match.play_match()
+
+test_match.winner.update_ranking_points(1000)
+
+print(f"The match winner is: {test_match.winner} and their updated ranking points: {test_match.winner.updated_ranking_points}")
