785ec3c9500c748c4f762afbd014dcb3301b3477
plcr9 <paulcooperpc9@gmail.com>
2023-08-24 17:50:10 +0100

    Update Ranking Points fault corrected

--- play_tennis.py
+++ play_tennis.py
@@ -9,4 +9,4 @@ test_match.play_match()
 
 test_match.winner.update_ranking_points(1000)
 
-print(f"The match winner is: {test_match.winner} and their updated ranking points: {test_match.winner.updated_ranking_points}")
+print(f"The match winner is: {test_match.winner} and their updated ranking points: {test_match.winner.ranking_points}")
